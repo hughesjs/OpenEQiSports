447de42d01900381f20773c028d4935ddcbafeec
James Hughes <james@pyrosoftsolutions.co.uk>
2024-02-03 23:23:44 +0000

    Added auto DI for all ContentPages

--- src/OpenEqiSports/DependencyInjection/DependencyInjectionExtensions.cs
+++ src/OpenEqiSports/DependencyInjection/DependencyInjectionExtensions.cs
@@ -5,7 +5,21 @@ public static class DependencyInjectionExtensions
     public static IServiceCollection AddFrontend(this IServiceCollection services)
     {
         services.AddSingleton<AppShell>();
-        services.AddSingleton<MainPage>();
+        services.AddAllScreens();
+        return services;
+    }
+    
+    public static IServiceCollection AddAllScreens(this IServiceCollection services)
+    {
+        List<Type> screens = typeof(App).Assembly.GetTypes()
+            .Where(x => x.IsSubclassOf(typeof(ContentPage)))
+            .ToList();
+        
+        foreach (Type screen in screens)
+        {
+            services.AddSingleton(screen);
+        }
+        
         return services;
     }
 }
